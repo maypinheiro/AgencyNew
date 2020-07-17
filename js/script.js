9faef03beb871bd837867751fb434bb7723b3b90
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-07-17 16:35:51 -0300

    Carrosel principal
    Estrutura do Carrosel principal ok

--- js/script.js
+++ js/script.js
@@ -6,6 +6,14 @@ $(document).ready(function() {
 
 
     });
+    /* Carrosel Principal */
+
+    $('.owl-carousel').owlCarousel({
+        items: 1, // para mostrar 1 imagem por vez
+        lazyLoad: true,
+        loop: true,
+        margin: 10
+    });
 
 
 });
