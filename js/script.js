8ab752f7feef66400d76beddcfb5a25b66358ba7
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-07-17 14:10:05 -0300

    Abrindo e Fechando Menu Mobile
    Abrindo e Fechando Menu Mobile - Concluida toda parte de MENU.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#nav-toggle").click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $(".header-collapse").toggleClass("active");
+
+
+    });
+
+
+});
