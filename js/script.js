483d32063fcc3250b71d7047ead60cf0dcf1329f
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-07-17 21:15:19 -0300

    Carousel Principal Speed
    Carousel Principal Speed

--- js/script.js
+++ js/script.js
@@ -8,14 +8,20 @@ $(document).ready(function() {
     });
     /* Carrosel Principal */
 
-    $('.owl-carousel').owlCarousel({
+    $('#carousel_principal').owlCarousel({
         items: 1, // para mostrar 1 imagem por vez
         lazyLoad: true,
         loop: true,
         margin: 10,
         nav: true,
+        navSpeed: 1000,
         navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-        dots: true
+        dots: true,
+        dotsSpeed: 1000,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsiveRefreshRate: 10
+
     });
 
 
